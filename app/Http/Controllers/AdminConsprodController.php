7be75de495b64080139854294a767ae055423d01
mohamxrd2 <pro.mohamxrd@gmail.com>
2024-05-03 09:49:09 +0000

    add

--- app/Http/Controllers/AdminConsprodController.php
+++ app/Http/Controllers/AdminConsprodController.php
@@ -10,7 +10,7 @@ class AdminConsprodController extends Controller
     //
     public function index()
     {
-        $consommations = Consommation::where('type', 'produits')->paginate(10);
+        $consommations = Consommation::where('type', 'produits')->orderBy('created_at', 'DESC')->paginate(10);
 
         return view('admin.conso-produit', ['consommations' => $consommations]);
 
