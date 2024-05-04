e5b0b299999dd6a7affc5176255989a2a4e911cd
mohamxrd2 <pro.mohamxrd@gmail.com>
2024-05-04 12:48:03 +0000

    add

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,20 +14,20 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        // // Données de l'administrateur à insérer dans la table 'admins'
-        // $adminData = [
-        //     'name' => 'Mohamed',
-        //     'username' => 'admin',
-        //     'password' => Hash::make('admin'), // Hashage du mot de passe
-        //     'phonenumber' => '0576507639',
+        // Données de l'administrateur à insérer dans la table 'admins'
+        $adminData = [
+            'name' => 'Mohamed',
+            'username' => 'admin',
+            'password' => Hash::make('admin'), // Hashage du mot de passe
+            'phonenumber' => '0576507639',
 
-        //     // Autres champs si nécessaire
-        // ];
+            // Autres champs si nécessaire
+        ];
 
         // // Création d'un nouvel administrateur dans la table 'admins'
         // Admin::create($adminData);
 
-        Admin::factory(10)->create();
+        
 
         // Vous pouvez ajouter plus d'administrateurs si nécessaire
     }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,13 +8,13 @@
     @auth('admin')
         @if (Auth::guard('admin')->user()->admin_type == 'agent')
             <!-- Contenu spécifique à l'agent -->
-            <h1>Tableau de bord de l'agent</h1>
+         
             <!-- Autres éléments spécifiques à l'agent -->
 
             @include('admin.dashboard.dashbord_agent')
             @else
             <!-- Contenu spécifique à l'administrateur général -->
-            <h1>Tableau de bord de l'administrateur général</h1>
+            
             <!-- Autres éléments spécifiques à l'administrateur général -->
 
             @include('admin.dashboard.dashbord_admin')
