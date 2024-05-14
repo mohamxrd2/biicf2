9a50509b6c67c4483f879b7778ce93d2e4d89032
ahondjo kwonjin <k.ahondjo98@gmail.com>
2024-05-14 12:28:51 +0000

    chance

--- resources/views/admin/services.blade.php
+++ resources/views/admin/services.blade.php
@@ -89,7 +89,7 @@
                                     class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                     <th scope="row"
                                         class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                                        <a href="{{ route('produit.pubShow', $service->name) }}" class="flex items-center">
+                                        <a href="{{ route('produit.pubShow', $service->id) }}" class="flex items-center">
 
                                             <img class="w-10 h-10 rounded-md"
                                                 src="{{ $service->photo ? $service->photo : asset('img/noimg.jpeg') }}"
