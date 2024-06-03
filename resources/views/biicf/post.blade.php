892f56ba616d82a66461fc45450ea49b9de6b7a6
ahondjo kwonjin <k.ahondjo98@gmail.com>
2024-06-03 12:10:45 +0000

    chance

--- resources/views/biicf/post.blade.php
+++ resources/views/biicf/post.blade.php
@@ -180,9 +180,9 @@
                                                     class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
                                                     placeholder="Commune " />
                                             </div>
-                                           
 
-                                           
+
+
                                             <div class="relative z-0 w-full mb-5 group">
                                                 <textarea id="floating_description" name="description"
                                                     class="py-3 px-4 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
@@ -502,7 +502,7 @@
 
                                                             <div class="mt-6 flex justify-center gap-x-4">
                                                                 <form
-                                                                    action="{{ route('biicf.pubdelete', $produit->id) }}"
+                                                                    action="{{ route('biicf.pubdeleteBiicf', $produit->id) }}"
                                                                     method="POST">
                                                                     @csrf
                                                                     @method('DELETE')
